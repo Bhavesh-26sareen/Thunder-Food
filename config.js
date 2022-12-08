2e03673970f0a86962f388e9f7b43b51db4cfa08
Bhavesh-26sareen <sareenbhavesh26@gmail.com>
2022-12-08 22:37:22 +0530

    f

--- config.js
+++ config.js
@@ -9,3 +9,13 @@ module.exports = {
     RAZOR_PAY_KEY_ID: envar.RAZOR_PAY_KEY_ID,
     RAZOR_PAY_KEY_SECRET: envar.RAZOR_PAY_KEY_SECRET
 }
+// const TWO_HOURS = 1000 * 60 * 60 * 24;
+// module.exports = {
+//     MONGODB: 'mongodb+srv://prateek-new:itsprateekbansal@cluster0.g5cto.mongodb.net/merng?retryWrites=true&w=majority',
+//     PORT: 3000,
+//     SESS_LIFETIME: TWO_HOURS,
+//     SESS_NAME: 'sid',
+//     SESS_SECRET: 'iamprateek,hello',
+//     RAZOR_PAY_KEY_ID: 'rzp_test_tnPHGLOSuXuFNE',
+//     RAZOR_PAY_KEY_SECRET: 'tvIv3jspaAK7ZCVPQ19U12vJ',
+// }
